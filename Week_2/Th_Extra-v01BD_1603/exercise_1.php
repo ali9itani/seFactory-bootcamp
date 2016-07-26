84b49a89c48d1066d4f16af9efd6a52443338e47
ali itani <ali9itani@gmail.com>
2016-07-26 17:18:08 +0200

    new edits for exercise 1 and 2

--- Week_2/Th_Extra-v01BD_1603/exercise_1.php
+++ Week_2/Th_Extra-v01BD_1603/exercise_1.php
@@ -1,6 +1,5 @@
 <?php
-
-$persons = array(); 
+$persons = array();
 echo "enter black or white, any key to stop insertion\n";
 $person_count=1;
 while(true){
@@ -33,52 +32,54 @@ else
 {
 	$suggests[$person_count-1] = "white";
 }
-
-
-while ($person_count>1)
+//looping through persons
+while($person_count>1)
 {
-
 	//next guy turn
 	$person_count--;
- 
- 	$black_count=0;
 	//to hear the ones before, starting from next person until the element before last one
 	//since last one is just a hint
-	for($x=$person_count ; $x < sizeof($suggests)-1;  $x++)
+	$black_count=0;
+	for($x=$person_count; $x < sizeof($suggests)-1; $x++)
 	{	if($suggests[$x] == "black")
 		{
 			$black_count++;
 		}
-
 	}
 	//to look at all black hats in front of him
-	for($x=$person_count-2; $x >=0  ;  $x--)
+	for($x=$person_count-2; $x >=0 ; $x--)
 	{
-	 	
 		if($persons[$x] == "black")
 		{
 			$black_count++;
 		}
 	}
-
 	//if blacks are odd && our count is even  
-	//if blacks are even && our count is odd
-	//then I am black
-	if(($suggests[sizeof($suggests)-1]=="black" &&  ($black_count%2==0))  
-		|| ($suggests[sizeof($suggests)-1]=="white" &&  ($black_count%2!=0)))
-		{
-			$suggests[$person_count-1] = "black";
-		}
+	//or if blacks are even && our count is odd
+	//then current is black
+	if((end($suggests)=="black" && ($black_count%2==0))
+	|| (end($suggests)=="white" && ($black_count%2!=0))
+	)
+	{
+		$suggests[$person_count-1] = "black";
+	}
 	else
-		{	 
-			$suggests[$person_count-1] = "white";		 
-		}
+	{
+		$suggests[$person_count-1] = "white";
+	}
 }
-
+//printing suggests
 for($x=0 ; $x < sizeof($suggests) ; $x++)
 {
 	echo $suggests[$x]." ";
 }
-
 echo "\n";
+if(end($suggests)==end($persons))
+{
+	echo "no mistakes\n";
+}
+else
+{
+	echo "1 mistakes\n";
+}
 ?>
--- Week_2/Th_Extra-v01BD_1603/exercise_2.php
+++ Week_2/Th_Extra-v01BD_1603/exercise_2.php
@@ -1,64 +1,48 @@
 <?php
 $winning_word = "error msg: nothing found";
 $winning_word_size = 0;
- $keyboard_rows = array(" qwertyuiop " , " asdfghjkl " , " zxcvbnm " );
+$keyboard_rows = array("qwertyuiop" , "asdfghjkl" , "zxcvbnm" );
 $words_list = array();
 //open a file just for read only(r)
 $data_file = fopen("data.txt", "r") or die("Unable to open file!");
 // push to array one line until end-of-file
-while(!feof($data_file)) {
-  array_push($words_list , fgets($data_file)); 
+while(!feof($data_file))
+{
+	array_push($words_list , fgets($data_file));
 }
-
- 
 //first check word by word
 //for each word check if it is made of one row only
 //if its the first word, put it in winning_word
 //else compare it to winning_word and choose the winner
-
 for($i=0 ; $i < sizeof($words_list)-1; $i++)
 {
-
-	  
-			//check if the word is not just in one row
-			if(check_if_not_valid_word( $keyboard_rows,$words_list[$i]))	
-			{
-				continue;
-			}	
-			 
-			if(strlen ($words_list[$i])>$winning_word_size)
-				{	$winning_word = $words_list[$i] ;
-					$winning_word_size = strlen ($words_list[$i]);
-				}	
+	//check if the word is not just in one row  so it is not valid
+	if(check_if_not_valid_word($keyboard_rows,$words_list[$i]))
+	{
+		continue;
+	}
+	if(strlen ($words_list[$i])>$winning_word_size)
+	{
+		$winning_word = $words_list[$i];
+		$winning_word_size = strlen ($words_list[$i]);
+	}
 }
-
-
-
 echo $winning_word;
-
-
-
 
 function check_if_not_valid_word( $keyboard_rows,$value)
 { 				
-		
-
-
-				if (((levenshtein($keyboard_rows[0],$value )+(10-strlen ($value)))<10
-					 &&((levenshtein($keyboard_rows[1],$value )+(9-strlen ($value)))<9 || ((levenshtein($keyboard_rows[2],$value )+(7-strlen ($value)))<7 )))
-					||
-					((levenshtein($keyboard_rows[2],$value )+(7-strlen ($value)))<7
-					 &&((levenshtein($keyboard_rows[1],$value )+(9-strlen ($value)))<9 || ((levenshtein($keyboard_rows[0],$value )+(10-strlen ($value)))<10 )))
-					||
-					((levenshtein($keyboard_rows[1],$value )+(9-strlen ($value)))<9
-					 &&((levenshtein($keyboard_rows[0],$value )+(10-strlen ($value)))<10 || ((levenshtein($keyboard_rows[2],$value )+(7-strlen ($value)))<7 )))
-
-					)
-					return true;
-			 		
-					return false;
+	if(((levenshtein($keyboard_rows[0],$value )+(10-strlen ($value)))<10 &&
+		((levenshtein($keyboard_rows[1],$value )+(9-strlen ($value)))<9 || ((levenshtein($keyboard_rows[2],$value )+(7-strlen ($value)))<7 )))
+		||
+		((levenshtein($keyboard_rows[2],$value )+(7-strlen ($value)))<7 &&
+		((levenshtein($keyboard_rows[1],$value )+(9-strlen ($value)))<9 || ((levenshtein($keyboard_rows[0],$value )+(10-strlen ($value)))<10 )))
+		||
+		((levenshtein($keyboard_rows[1],$value )+(9-strlen ($value)))<9 &&
+		((levenshtein($keyboard_rows[0],$value )+(10-strlen ($value)))<10 || ((levenshtein($keyboard_rows[2],$value )+(7-strlen ($value)))<7 ))))
+		{
+			return true;
+		}
+		return false;
 }
-
 fclose($data_file);
-
 ?>
