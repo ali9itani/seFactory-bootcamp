f0f133dc73404a1d12ea2f1f3b8f54e4f3474d44
ali itani <ali9itani@gmail.com>
2016-07-27 11:03:30 +0200

    exr2: <500 characters

--- Week_2/Th_Extra-v01BD_1603/exercise_2.php
+++ Week_2/Th_Extra-v01BD_1603/exercise_2.php
@@ -1,23 +1,15 @@
 <?php
-$winning_word = "error msg: nothing found";
-$winning_word_size = 0;
-$keyboard_rows = array("qwertyuiop" , "asdfghjkl" , "zxcvbnm" );
-$words_list = array();
-$data_file = fopen("data.txt", "r") or die("Unable to open file!");
-while(!feof($data_file))
-{
-	array_push($words_list , fgets($data_file));
+$sol = "not found";
+$wrd_size = 0;
+$querty = array("qwertyuiop","asdfghjkl","zxcvbnm");
+$wrds_ls = file("data.txt");
+for($i=0;$i<sizeof($wrds_ls);$i++){
+  if(similar_text($querty[0],$wrds_ls[$i]) ? (similar_text($querty[1],$wrds_ls[$i])||similar_text($querty[2],$wrds_ls[$i])) : (similar_text($querty[1],$wrds_ls[$i])&&similar_text($querty[2],$wrds_ls[$i])))
+    continue;
+  if(strlen($wrds_ls[$i])>$wrd_size){
+    $sol = $wrds_ls[$i];
+    $wrd_size = strlen($wrds_ls[$i]);
+  }
 }
-for($i=0 ; $i < sizeof($words_list)-1; $i++)
-{
-	if(similar_text($keyboard_rows[0],$words_list[$i] ) ? (similar_text($keyboard_rows[1],$words_list[$i] ) || similar_text($keyboard_rows[2],$words_list[$i] )) : (similar_text($keyboard_rows[1],$words_list[$i] ) && similar_text($keyboard_rows[2],$words_list[$i] )))
-		continue;
-	if(strlen ($words_list[$i])>$winning_word_size)
-	{
-		$winning_word = $words_list[$i];
-		$winning_word_size = strlen ($words_list[$i]);
-	}
-}
-echo $winning_word;
-fclose($data_file);
+echo $sol;
 ?>
