366076034b9df5f05f91b82d0373c9e642d814c6
ali itani <ali9itani@gmail.com>
2016-08-25 15:53:22 +0200

    finished min requirments

--- Week_6/Day_29_thursday_Assignments-v01BD_1608/js/main.js
+++ Week_6/Day_29_thursday_Assignments-v01BD_1608/js/main.js
@@ -1,1 +1,23 @@
+<!-- 
+//comment tag to "hide" scripts from browsers without support
 
+hanoi(8, 'source-tower', 'temporary-tower', 'destination-tower');
+
+function hanoi (disc, source, temp, destination) {  
+	if (disc > 0) {
+	   hanoi(disc - 1, source, destination, temp);
+	   var status = 'Move disc ' + disc + ' from ' + source + ' to ' + destination;
+ 	   PrintNewStatus(status);
+	   hanoi(disc - 1, temp, source, destination);
+ 	}
+}
+function PrintNewStatus(status) {
+    var new_paragraph = document.createElement('p');
+    new_paragraph.textContent = status;
+    var status_box = document.getElementById("status-box");
+    status_box.insertBefore(new_paragraph, status_box.firstChild);
+}
+function redrawDisk() {
+
+}
+//-->
