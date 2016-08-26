d12884e72884042a03081698f16aa297ab2e836f
ali itani <ali9itani@gmail.com>
2016-08-26 12:18:09 +0200

    finished from drawing towers/disks -  still need to move disks one by one

--- Week_6/Day_29_thursday_Assignments-v01BD_1608/js/main.js
+++ Week_6/Day_29_thursday_Assignments-v01BD_1608/js/main.js
@@ -1,13 +1,47 @@
 <!-- 
 //comment tag to "hide" scripts from browsers without support
+drawDisks();
 
-hanoi(8, 'source-tower', 'temporary-tower', 'destination-tower');
+hanoi(8, 'tower-source', 'tower-temporary', 'tower-destination');
 
+
+var source_tower = 0;
+var temporary_tower = 0;
+var destination_tower = 0;
+
+function drawDisks() {
+	var disk_number = 1;
+
+	while(disk_number < 9) {
+		
+
+		var height = disk_number * 10;
+		var width = disk_number * 15;
+
+		var new_disk = document.createElement('div');
+		new_disk.setAttribute("class", "oval");
+		new_disk.setAttribute("id", "disk"+disk_number);
+		new_disk.setAttribute("style", "width: "+width+"px ;" + "height: "+height+"px ; z-index:"+(8-disk_number)+"; left:"+5*(8-disk_number)+"px;top:"+(((5*(disk_number+5))*(8-disk_number)))+"px ;");
+
+	    var source_tower = document.getElementById("tower-source");
+	    source_tower.appendChild(new_disk);
+
+	    disk_number++;
+	}
+
+}
 function hanoi (disc, source, temp, destination) {  
 	if (disc > 0) {
 	   hanoi(disc - 1, source, destination, temp);
+
+	   //moving disk from one place to another
 	   var status = 'Move disc ' + disc + ' from ' + source + ' to ' + destination;
+
+
+	   changeDiskPlace(disc, destination);
+	   
  	   PrintNewStatus(status);
+
 	   hanoi(disc - 1, temp, source, destination);
  	}
 }
@@ -17,7 +51,10 @@ function PrintNewStatus(status) {
     var status_box = document.getElementById("status-box");
     status_box.insertBefore(new_paragraph, status_box.firstChild);
 }
-function redrawDisk() {
+function changeDiskPlace(disc, destination) {
+ 	   var disk_display = document.getElementById("disk"+disc);
+ 	   var destination_display = document.getElementById(destination);
+	   destination_display.insertBefore(disk_display, destination_display.firstChild);
 
 }
 //-->
