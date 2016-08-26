212ee1361bd17abe438687dfedac42fe8c38c749
ali itani <ali9itani@gmail.com>
2016-08-26 14:06:20 +0200

    added a start button, disks start moving, but moves still not clear

--- Week_6/Day_29_thursday_Assignments-v01BD_1608/js/main.js
+++ Week_6/Day_29_thursday_Assignments-v01BD_1608/js/main.js
@@ -13,7 +13,7 @@ function drawDisks() {
 		var new_disk = document.createElement('div');
 		new_disk.setAttribute("class", "oval");
 		new_disk.setAttribute("id", "disk"+disk_number);
-		new_disk.setAttribute("style", "width: "+width+"px ;" + "height: "+height+"px ; z-index:"+(8-disk_number)+"; left:"+5*(8-disk_number)+"px;top:"+(((5*(disk_number+5))*(8-disk_number)))+"px ;");
+		new_disk.setAttribute("style", "width: "+width+"px ;" + "height: "+height+"px ; z-index:"+(8-disk_number)+"; left:"+7*(8-disk_number)+"px;bottom:"+20*(8-disk_number)+"px ;");
 
 		var source_tower = document.getElementById("tower-source");
 		source_tower.appendChild(new_disk);
@@ -25,14 +25,17 @@ function drawDisks() {
 function hanoi (disc, source, temp, destination) {
 
 	if (disc > 0) {
+
 		hanoi(disc - 1, source, destination, temp);
 
 		//moving disk from one place to another
 		var status = 'Move disc ' + disc + ' from ' + source + ' to ' + destination;
-		
+
 		changeDiskPlace(disc, destination);
+
 		PrintNewStatus(status);
-		hanoi(disc - 1, temp, source, destination);
+		//hanoi(disc - 1, temp, source, destination);
+		setTimeout(function() {hanoi(disc - 1, temp, source, destination); }, 1000)
 	}
 }
 
@@ -55,8 +58,10 @@ function changeDiskPlace(disc, destination) {
 	destination_display.insertBefore(disk_display, destination_display.firstChild);
 }
 
-drawDisks();
+function startGame(){
+	hanoi(8, 'tower-source', 'tower-temporary', 'tower-destination');
+}
 
-hanoi(8, 'tower-source', 'tower-temporary', 'tower-destination');
+drawDisks();
 
 //-->
