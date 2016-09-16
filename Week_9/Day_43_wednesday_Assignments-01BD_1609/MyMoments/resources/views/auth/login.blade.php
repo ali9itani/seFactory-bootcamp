0cee007c25103f07231a5463ddaf16b5d59d132b
ali itani <ali9itani@gmail.com>
2016-09-16 14:19:51 +0200

    added register link

--- Week_9/Day_43_wednesday_Assignments-01BD_1609/MyMoments/resources/views/auth/login.blade.php
+++ Week_9/Day_43_wednesday_Assignments-01BD_1609/MyMoments/resources/views/auth/login.blade.php
@@ -57,6 +57,7 @@
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">
                                     Forgot Your Password?
                                 </a>
+                                <a  href="{{ url('/register') }}">Register</a>
                             </div>
                         </div>
                     </form>
